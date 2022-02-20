252ccaec8a39c1381e081b93c1f2a2e362114a3e
igor_bashuk <kiidtt1@gmail.com>
2022-02-20 19:44:58 +0200

    feat: use controller for index route

--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [\App\Http\Controllers\IndexController::class, 'index'])->name('home');
 
 // Обработка формы с использованием middleware
 //Route::get('/form', function () {
