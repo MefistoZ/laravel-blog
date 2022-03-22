212de79f1b7d633d1cb963ce79a5ed5c4630370e
Igor_Bashuk <igor@redchameleon.com.ua>
2022-03-22 11:51:47 +0200

    feat: delete and create posts directory for store seed images

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Database\Factories\AdminUserFactory;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Storage;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,6 +15,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+
+        Storage::deleteDirectory('public/posts');
+        Storage::makeDirectory('public/posts');
+
         \App\Models\User::factory(10)->create();
         \App\Models\Post::factory(10)->create();
 
